9403386c905303e68fcedab6e5785f520d219067
lkhilton <laura.k.hilton@gmail.com>
2025-07-22 14:50:03 -0700

    Document the dzsig models

--- R/data.R
+++ R/data.R
@@ -1,6 +1,6 @@
 #' Primary COO classifier
 #'
-#' An 'lm' object containing a 21 gene COO classifier, developed from the GOYA 
+#' An 'lm' object containing a 21 gene COO classifier, developed from the GOYA
 #' dataset. Two versions are available, for data processed with either
 #' IGIS3.0 or IGIS4.0
 #'
@@ -14,3 +14,19 @@
 
 #' @rdname coo_GOYA_21gene
 "coo_GOYA_21gene_igis4"
+
+
+#' Primary DZsig classifier
+#'
+#' An 'lm' object containing a 29 gene DZsig classifier, developed from the GAMBL DLBCL
+#' dataset. Two versions are available, for data processed with either
+#' IGIS3.0 (RefSeq IDs) or IGIS4.0 (Ensembl IDs).
+#'
+#' @name dzsig_GAMBL_29gene
+#' @format An object of class 'lm'. Coefficients can be viewed by accessing
+#'    the '$coefficients' slot.
+"dzsig_GAMBL_29gene_igis3"
+
+
+#' @rdname dzsig_GAMBL_29gene
+"dzsig_GAMBL_29gene_igis4"
